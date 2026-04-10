e70a2072cc33814b59902b051239371859a15db8
Shivin Pruthi <pruthi.shivin30@gmail.com>
2026-04-10 18:52:24 +0530

    some errors

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "process.env.REACT_APP_API_URL;";
+const API_URL = process.env.REACT_APP_API_URL;;
 
 export const signupUser = async (data) => {
   return axios.post(`${API_URL}/signup`, data);
