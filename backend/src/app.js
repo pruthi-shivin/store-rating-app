a6a4e8b5c7b1abc98a6899c33dab856804a269d7
Shivin Pruthi <pruthi.shivin30@gmail.com>
2026-04-10 20:53:30 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
