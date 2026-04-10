6aa12353c3968dc4becbc90f4ed94d439b3acb95
Shivin Pruthi <pruthi.shivin30@gmail.com>
2026-04-10 22:41:37 +0530

    sc fix

--- backend/src/controllers/storeController.js
+++ backend/src/controllers/storeController.js
@@ -4,27 +4,25 @@ export const getAllStores = (req, res) => {
   const userId = req.user.id;
   const { search = "" } = req.query;
 
-  const query = `
+  let query = `
     SELECT 
-    s.id,
-    s.name,
-    s.address,
-    ROUND(AVG(r.rating), 1) AS overallRating,
-    ur.rating AS userRating,
-    ur.id AS userRatingId
+      s.id,
+      s.name,
+      s.address,
+      ROUND(AVG(r.rating), 1) AS overallRating,
+      ur.rating AS userRating,
+      ur.id AS userRatingId
 
     FROM stores s
 
     LEFT JOIN ratings r 
-    ON s.id = r.store_id
+      ON s.id = r.store_id
 
     LEFT JOIN ratings ur 
-    ON s.id = ur.store_id AND ur.user_id = ?
+      ON s.id = ur.store_id AND ur.user_id = ?
+  `;
 
-    GROUP BY s.id
-    `;
-
-  const values = [userId];
+  let values = [userId];
 
   if (search) {
     query += `
@@ -33,14 +31,14 @@ export const getAllStores = (req, res) => {
     values.push(`%${search}%`, `%${search}%`);
   }
 
-  query += " GROUP BY s.id";
+  query += ` GROUP BY s.id`;
 
-  db.query(query, [userId], (err, result) => {
-  if (err) {
-    console.log(err);
-    return res.status(500).json({ error: err.message });
-  }
+  db.query(query, values, (err, result) => {
+    if (err) {
+      console.log("DB ERROR:", err);
+      return res.status(500).json({ error: err.message });
+    }
 
-  res.json(result);
-}
-);}
+    res.json(result);
+  });
+};
